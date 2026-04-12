afe3eb7a63aa623105310efba6d986eb02a50d41
anonymousEROS <dovala69@gmail.com>
2026-04-11 22:41:39 -0700

    server config

--- cmd/server/serverTCP.go
+++ cmd/server/serverTCP.go
@@ -6,15 +6,35 @@ import (
     "log"
     "net"
     "strings"
+    "os"
+    "encoding/json"
 )
 
+type config struct{
+	PORT string `json:"PORT"` 
+}
+
 func main() {
-    var PORT = ":8000"		
-    ln, err := net.Listen("tcp", PORT)
+    var Cfg config
+
+    file, err := os.Open("../config.json")
+    if err != nil{
+	log.Fatal(err)
+    }
+    defer file.Close()
+
+    decoder := json.NewDecoder(file)
+    err = decoder.Decode(&Cfg)
+    if err != nil{
+	    fmt.Println("Err decoding JSON:", err)
+	    return 
+    }
+
+    ln, err := net.Listen("tcp", Cfg.PORT)
     if err != nil {
         log.Fatal(err)
     }
-    fmt.Println("Listening on port", PORT)
+    fmt.Println("Listening on port", Cfg.PORT)
     conn, err := ln.Accept()
     if err != nil {
         log.Fatal(err)
