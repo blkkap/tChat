87f11abf9419d86549de9dafb4bc93cc4b43a8b8
anonymousEROS <dovala69@gmail.com>
2026-04-12 21:02:17 -0700

    input chan

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"fmt"
+	
+	"os/signal"
 	"log"
 	"os"
+	"bufio"
 	"github.com/gorilla/websocket"
 	"encoding/json"
 )
 
 var Cfg config
+var in = bufio.NewReader(os.Stdin)
 
 
 type config struct{
@@ -31,25 +34,68 @@ func getConfig(){
 	}
 }
 
+// Func that gets users input
+func getInput(input chan string){
+	results, err := in.ReadString('\n')
+	if err != nil{
+		log.Println(err)
+		return
+	}
+	input <- results
+}
+
 
 func main(){
 	getConfig()
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+	input := make(chan string, 1)
+	go getInput(input)
 	var url = Cfg.URL
 	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
 	if err != nil{
 		log.Fatal("dial:", err)
 	}
 	defer conn.Close()
-	/*for{
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Msg to send")
-		text, _ := reader.ReadString('\n')
-		fmt.Fprintf(conn,text + "\n")
-		message,_ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Print()
-	}*/ 
-	message := Cfg.USERNAME + ": Hello"
-	err = conn.WriteMessage(websocket.TextMessage, []byte(message))
+
+	done := make(chan struct{})
+	go func(){
+		defer close(done)
+		for{
+			_, message, err := conn.ReadMessage()
+			if err != nil{
+				log.Println("ReadMessage() err:", err)
+				return
+			}
+			log.Printf("Recieved: %s", message)
+		}
+	}()
+	for {
+		select{
+		case <- done:
+			return
+		case t := <-input:
+			err := conn.WriteMessage(websocket.TextMessage, []byte(t))
+			if err != nil{
+				log.Println("write err:", err)
+				return 
+			}
+			go getInput(input)
+		case <-interrupt:
+			log.Println("Caught interrupt signal")
+			err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+
+			if err != nil{
+				log.Println("Write close err:", err)
+				return
+			}
+			return
+		}	
+
+	}
+	//message := Cfg.USERNAME + ": Hello"
+	//err = conn.WriteMessage(websocket.TextMessage, []byte(message))
+	/*
 	if err != nil{
 		log.Fatal(err)
 	}
@@ -62,4 +108,5 @@ func main(){
 		}
 		fmt.Printf("[%s]: %s\n",Cfg.USERNAME, message)
 	}
+	*/
 }
