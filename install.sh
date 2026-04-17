c1a874451fc74f884db5007eaefb5cc0e4ee2847
anonymousEROS <dovala69@gmail.com>
2026-04-17 15:11:26 -0700

    shell script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+VERSION="v0.1.0"
+OS="$(uname | tr '[:upper:]' '[:lower:]')"
+ARCH="amd64"
+
+
+if [["$(uname -m)" == "arm64"]]; then
+    ARCH="arm64"
+fi
+
+URL = "https://github.com/yourname/tchat/releases/download/$VERSION/tchat-$OS-$ARCH.tar.gz"
+
+curl -L $URL -o tchat.tar.gz
+tar -xzf tchat.tar.gz
+chmod +x tchat
+
+sudo mv tchat /usr/local/bin/	
+
+echo "Installed! run with: tchat"
